12b9ee6a1e67baa86f93ce4fb9e288b7c0538a65
Abderrahman Elkhalil <aelkhali@e1r6p5.1337.ma>
2023-10-15 17:47:41 +0100

    CGI Execution using file for communication

--- cgi.cpp
+++ cgi.cpp
@@ -1,20 +1,21 @@
 # include "cgi.hpp"
 
 /*  CGI Constructors    */
-cgi::cgi    ( server_data const& server, int locationIndex )
+cgi::cgi    ( server_data const& server, location_data const& location )
 {
-    _cgiBinPath = server.getLocations()[locationIndex].getCgiPath();
+    _cgiBinPath = location.getCgiPath();
     errorMsg = "unknown Error";
     _pid = -1;
-    pipeFds[0] = -1;
     _envp = NULL;
+    outputCGIFile = -1;
+    _cgiOutputPath = "/tmp/.serveX_" + to_string(_initTime()) + "__.cgi";
     _initServerEnvVariables(server);
 }
 cgi::cgi    ( void ) {}
 cgi::~cgi   ( void )
 {
-    if (pipeFds[0] != -1)
-        close(pipeFds[0]);
+    if (outputCGIFile != -1)
+        close(outputCGIFile);
     if (_envp)
     {
         for (size_t i = 0; _envp[i]; ++i)
@@ -27,18 +28,17 @@ cgi::~cgi   ( void )
 /* CGI Core Method  */
 bool    cgi::executeCgi( request const& req)
 {
-    if (!_setupCgiEnvs(req) || !_initPipeFds() || !_executeCgiScript(req))
+    if (!_setupCgiEnvs(req) || !_initOutputFile() || !_executeCgiScript(req._body.bodycontent, req))
         return false;
-    /*  if success  */
     return true;
 }
 
-/*  CGI Private Helper Methods  */
+/*  ____________CGI Private Helper Methods___________  */
+
 // Init Envirenment Variables for CGI and convert them to key=value for execve()
 bool    cgi::_setupCgiEnvs( request const& req)
 {
     _initRequestEnvVariables(req);
-
     /*  Convert the environment map to the required format (name=value) inside a vector and set it in envp */
     std::map<std::string, std::string>::const_iterator it;
     int i = 0;
@@ -66,53 +66,70 @@ bool    cgi::_setupCgiEnvs( request const& req)
     }
     return true;
 }
-// Init communication between parent and child processes.
-bool    cgi::_initPipeFds( void) 
+// Init output file to write CGI output
+bool    cgi::_initOutputFile( void) 
 {
-    if (pipe(pipeFds) < 0)
+    outputCGIFile = open(_cgiOutputPath.c_str(), O_CREAT | O_WRONLY, 0666);
+    if (outputCGIFile < 0)
     {
-        errorMsg = "Failed to init the pipe for communication";
+        errorMsg = "Failed to create a file to hold CGI output";
         return false;
     }
     return true;
 }
 // execute the CGI script
-bool    cgi::_executeCgiScript( FILE *body )
+bool    cgi::_executeCgiScript( FILE *body, request const& req )
 {
     _pid = fork();
     if (_pid < 0)
     {
-        errorMsg = "Failed To Fork() to execute the CGI script";
+        errorMsg = "Fork() failed to execute the CGI script";
         return false;
     }
     if (_pid == 0)
     {
-        close(pipeFds[0]);
-        if (dup2(fileno(body), STDIN_FILENO) == -1)
+        if (req._method & POST)
+            if (dup2(fileno(body), STDIN_FILENO) < 0)
+                _exit(EXIT_FAILURE);
+        if (dup2(outputCGIFile, STDOUT_FILENO) < 0)
             _exit(EXIT_FAILURE);
-        if (dup2(pipeFds[1], STDOUT_FILENO) == -1)
-            _exit(EXIT_FAILURE);
-        char    *args[2];
-        args[0] = (char *)_script.c_str();
-        args[1] = NULL;
-        execve(_cgiBinPath.c_str(), args, _envp);
-        std::cerr << "Failed to Execute: " << _script << std::endl;
+        char* args[3];
+        args[0] = const_cast<char*>(_cgiBinPath.c_str());
+        args[1] = const_cast<char*>(_script.c_str());
+        args[2] = NULL;
+        execve(args[0], args, _envp);
         write(STDOUT_FILENO, "Status: 500\r\n\r\n", 15);
+        _exit(EXIT_FAILURE);
     }
     else
     {
-        close(pipeFds[1]);
+        std::time_t start_time = std::time(NULL);
         int  status;
-        waitpid(_pid, &status, WNOHANG);
+        while (waitpid(_pid, &status, WNOHANG) == 0)
+        {
+            std::time_t current_time = std::time(NULL);
+            if (current_time > start_time + MAX_EXEC_TIME)
+            {
+                if (kill(_pid, SIGKILL) < 0)
+                {
+                    errorMsg = "A system call method failed inside the cgi child process";
+                    return false;
+                }
+                errorMsg = "Execution time exceeded the server time limit";
+                return false;
+            }
+            usleep(10000);
+        }
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
         {
-            errorMsg = "A system call method failed inside the cgi child process";
+            errorMsg = "Failed to execute the requested (CGI)";
             return false;
         }
+        close(outputCGIFile);
+        outputCGIFile = -1;
     }
     return true;
 }
-
 // initialize envirenement variables releated to the server
 void    cgi::_initServerEnvVariables( server_data const& server )
 {
@@ -125,10 +142,16 @@ void    cgi::_initServerEnvVariables( server_data const& server )
 // initialize envirenement variables releated to the request
 void    cgi::_initRequestEnvVariables( request const& req )
 {
-    _script = "req.uri";
+    _script = req._path;
     _cgiEnvVars["SCRIPT_NAME"] = _script;
-    _cgiEnvVars["REQUEST_METHOD"] = req._method;
-    _cgiEnvVars["PATH_INFO"] = "";
+    if (req._method & GET)
+        _cgiEnvVars["REQUEST_METHOD"] = "GET";
+    else if (req._method & POST)
+        _cgiEnvVars["REQUEST_METHOD"] = "POST";
+    else
+        _cgiEnvVars["REQUEST_METHOD"] = "DELETE";
+
+    _cgiEnvVars["PATH_INFO"] = req._path;
         /*
             The extra path information, as given by the client. In other words, scripts can be accessed by their virtual pathname, 
             followed by extra information at the end of this path. The extra information is sent as PATH_INFO.
@@ -137,6 +160,7 @@ void    cgi::_initRequestEnvVariables( request const& req )
     _cgiEnvVars["QUERY_STRING"] = req._query;
     _cgiEnvVars["CONTENT_TYPE"] = req._body.bodyType; // For queries which have attached information, such as HTTP POST and PUT, this is the content type of the data.
     _cgiEnvVars["CONTENT_LENGTH"] = req._body.contentLength; // The length of the said content as given by the client.
+    // _cgiEnvVars["HTTP_COOKIE"] = req.cookie;
 }
 // convert port from int to string
 std::string cgi::_intToString( int value )
@@ -144,4 +168,11 @@ std::string cgi::_intToString( int value )
     std::stringstream ss;
     ss << value;
     return ss.str();
+}
+// init Time For File Creation
+unsigned long   cgi::_initTime( void )
+{
+    struct timeval _time;
+    gettimeofday(&_time, NULL);
+    return (unsigned long)_time.tv_usec * 10000;
 }
--- cgi.hpp
+++ cgi.hpp
@@ -5,40 +5,46 @@
 # include <vector>
 # include <map>
 # include <cstring>
-# include "request.hpp"
 # include <sys/wait.h>
+# include <sys/time.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <signal.h>
+# include <stdio.h>
+# include "request.hpp"
+
+#define MAX_EXEC_TIME 10
 
 class   cgi
 {
 
 public:
 
-    cgi             ( server_data const& , int ); // int: the location index inside the calling server
+    cgi             ( server_data const& , location_data const& ); // int: the location index inside the calling serve
     ~cgi            ( void );
     bool            executeCgi( request const& );
-    int             pipeFds[2];
     std::string     errorMsg;
+    int             _pid;
+    std::string     _cgiOutputPath;
 
 private:
 
     cgi                                 ( void );
     std::string                         _cgiBinPath;
     std::string                         _script;
-    int                                 _pid;
     std::map<std::string, std::string>  _cgiEnvVars;
     std::vector<std::string>            _tmpEnvs;
     char**                              _envp;
+    int                                 outputCGIFile;
 
     /*  Helper Private Functions */
     void            _initServerEnvVariables( server_data const& );
     void            _initRequestEnvVariables( request const& );
-    bool            _initPipeFds( void );
+    bool            _initOutputFile( void );
     bool            _setupCgiEnvs( request const& );
-    bool            _executeCgiScript( FILE *body );
+    bool            _executeCgiScript( FILE *body, request const& );
     std::string     _intToString( int );
+    unsigned long   _initTime( void );
 
 };
 #endif
